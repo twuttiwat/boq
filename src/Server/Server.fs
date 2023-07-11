fedb8d03634c2b2cd5d0201c7663a3497f3ba866
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2023-07-11 21:49:35 +0700

    add logging to functions in ServerApi

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -2,43 +2,25 @@ module Server
 
 open Fable.Remoting.Server
 open Fable.Remoting.Giraffe
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
 open Saturn
 
+open ServerApi
 open Shared
-
-module Storage =
-    let todos = ResizeArray()
-
-    let addTodo (todo: Todo) =
-        if Todo.isValid todo.Description then
-            todos.Add todo
-            Ok()
-        else
-            Error "Invalid todo"
-
-    do
-        addTodo (Todo.create "Create new SAFE project")
-        |> ignore
-
-        addTodo (Todo.create "Write your app") |> ignore
-        addTodo (Todo.create "Ship it !!!") |> ignore
-
-let todosApi =
-    { getTodos = fun () -> async { return Storage.todos |> List.ofSeq }
-      addTodo =
-        fun todo ->
-            async {
-                return
-                    match Storage.addTodo todo with
-                    | Ok () -> todo
-                    | Error e -> failwith e
-            } }
 
 let webApp =
     Remoting.createApi ()
+    |> Remoting.fromContext (fun (ctx: HttpContext) -> ctx.GetService<TodosApi>().Build())
     |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.fromValue todosApi
     |> Remoting.buildHttpHandler
+
+let serviceConfig (services: IServiceCollection) =
+    services
+        .AddSingleton<TodosApi>()
+        .AddLogging()
 
 let app =
     application {
@@ -46,6 +28,8 @@ let app =
         memory_cache
         use_static "public"
         use_gzip
+        service_config serviceConfig
+        host_config Env.configureHost
     }
 
 [<EntryPoint>]
---
+++ src/Server/ServerApi.fs
@@ -0,0 +1,49 @@
+﻿module ServerApi
+
+open Fable.Remoting.Server
+open Fable.Remoting.Giraffe
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
+open Saturn
+
+open Shared
+
+
+module Storage =
+    let todos = ResizeArray()
+
+    let addTodo (todo: Todo) =
+        if Todo.isValid todo.Description then
+            todos.Add todo
+            Ok()
+        else
+            Error "Invalid todo"
+
+    do
+        addTodo (Todo.create "Create new SAFE project")
+        |> ignore
+
+        addTodo (Todo.create "Write your app") |> ignore
+        addTodo (Todo.create "Ship it !!!") |> ignore
+
+let todosApi (logger: ILogger) =
+    { getTodos =
+        fun () ->
+            async {
+                logger.LogInformation("Executing {Function}", "getTodos")
+                return Storage.todos |> List.ofSeq
+            }
+      addTodo =
+        fun todo ->
+            async {
+                logger.LogInformation("Executing {Function}", "addTodo")
+                return
+                    match Storage.addTodo todo with
+                    | Ok () -> todo
+                    | Error e -> failwith e
+            } }
+
+type TodosApi(logger: ILogger<ITodosApi>, config: IConfiguration) =
+    member this.Build() : ITodosApi = todosApi logger
